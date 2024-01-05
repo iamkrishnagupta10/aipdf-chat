c650c32121dba174e39db173a453e2f044cdf7a6
Krishna Gupta <129900110+iamkrishnagupta10@users.noreply.github.com>
2024-01-04 16:55:01 -0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ import { eq } from "drizzle-orm";
 export default async function Home() {
   const { userId } = await auth();
   const isAuth = !!userId;
-  const isPro = await checkSubscription()
+  const isPro = await checkSubscription();
   let firstChat;
   if (userId) {
     firstChat = await db.select().from(chats).where(eq(chats.userId, userId));
@@ -20,24 +20,25 @@ export default async function Home() {
       firstChat = firstChat[0];
     }
   }
+
   return (
-    <div className="w-screen min-h-screen">
+    <div className="w-screen min-h-screen flex justify-center items-center flex-col">
+      <h1 className="text-5xl font-semibold text-center">
+        WXYZ.ai by Krishna Gupta
+      </h1>
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
         <div className="flex flex-col items-center text-center">
-          <h1 className="text-5xl font-semibold">
-            📄💬 ChatMagic 🚀🔮
-          </h1>
           <h2 className="mt-2 text-3xl font-medium">
             Transform Your PDFs into Conversations!
           </h2>
-          
+
           <div className="flex items-center mt-4 space-x-4">
             {isAuth && firstChat && (
-            <Link href={`/chat/${firstChat.id}`}>
-              <Button>
-                Go to Chats <ArrowRight className="ml-2" />
-              </Button>
-            </Link>
+              <Link href={`/chat/${firstChat.id}`}>
+                <Button>
+                  Go to Chats <ArrowRight className="ml-2" />
+                </Button>
+              </Link>
             )}
             <div className="ml-3">
               <SubscriptionButton isPro={isPro} />
@@ -46,14 +47,13 @@ export default async function Home() {
           </div>
 
           <p className="max-w-xl mt-1 text-lg text-slate-600 mt-4">
-            Dive into documents effortlessly. 
-            Our advanced AI assistant deciphers and chats about your PDF content, making research and understanding a breeze. 
-            🚀📄🤖
+            Dive into documents effortlessly.
+            Our advanced AI assistant deciphers and chats about your PDF content, making research and understanding a breeze. 🚀📄🤖
           </p>
-          
+
           <div className="w-full mt-4">
             {isAuth ? (
-              <FileUpload/>
+              <FileUpload />
             ) : (
               <Link href="/sign-in">
                 <Button>
